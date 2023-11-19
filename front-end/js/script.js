2a343f0c8a7281e4e9f8af13486e91cc4c0e3905
EL-Yasmina <yasminataif99@gmail.com>
2023-11-19 19:43:34 +0100

    ajouter description

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -2,7 +2,7 @@ let searchForm = document.querySelector('.header .search-form');
 
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
 }
@@ -10,34 +10,34 @@ document.querySelector('#menu-btn').onclick = () =>{
 let slides = document.querySelectorAll('.accueil .slide');
 let index = 0;
 
-function next(){
+function next() {
     slides[index].classList.remove('active');
     index = (index + 1) % slides.length;
     slides[index].classList.add('active');
 }
 
-function prev(){
+function prev() {
     slides[index].classList.remove('active');
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
 
 function validerMotDePasse() {
-        
+
     var inputMotDePasse = document.getElementById('mot_de_passe');
     var inputConfirmerMotDePasse = document.getElementById('confirmer_mot_de_passe');
-    var motDePasse = inputMotDePasse.value;        
+    var motDePasse = inputMotDePasse.value;
     var confirmerMotDePasse = inputConfirmerMotDePasse.value;
-    
+
     if (motDePasse !== confirmerMotDePasse) {
         inputConfirmerMotDePasse.setCustomValidity("Les mots de passe ne correspondent pas.");
         inputConfirmerMotDePasse.reportValidity();
-        return false; 
+        return false;
     }
-    return true; 
+    return true;
 }
 
-function cleanValidatorMotDePasse(){
+function cleanValidatorMotDePasse() {
     var inputConfirmerMotDePasse = document.getElementById('confirmer_mot_de_passe');
     inputConfirmerMotDePasse.setCustomValidity("");
 
@@ -46,41 +46,41 @@ function cleanValidatorMotDePasse(){
 
 
 function erreurMotDePasse() {
-        
+
     var inputMotDePasse = document.getElementById('mot_de_passe');
     var url = window.location.href;
-    if(!url.includes("login.php?erreur"))
-        return ;
+    if (!url.includes("login.php?erreur"))
+        return;
 
     inputMotDePasse.setCustomValidity("Les mots de passe ou l'email ne correspondent pas.");
     inputMotDePasse.reportValidity();
-    
+
 }
-function cleanValidatorMotDePasseLogin(){
+function cleanValidatorMotDePasseLogin() {
     var inputMotDePasse = document.getElementById('mot_de_passe');
     inputMotDePasse.setCustomValidity("");
 }
 
 
 function validerMotDePasseMonCompte() {
-    
+
     var inputMotDePasse = document.getElementById('new_mot_de_passe');
     var inputConfirmerMotDePasse = document.getElementById('confirmer_new_mot_de_passe');
-    var motDePasse = inputMotDePasse.value;        
+    var motDePasse = inputMotDePasse.value;
     var confirmerMotDePasse = inputConfirmerMotDePasse.value;
-    
+
     if (motDePasse.trim() === "" || confirmerMotDePasse.trim() === "") {
-        return true; 
+        return true;
     }
     if (motDePasse !== confirmerMotDePasse) {
         inputConfirmerMotDePasse.setCustomValidity("Les mots de passe ne correspondent pas.");
         inputConfirmerMotDePasse.reportValidity();
-        return false; 
+        return false;
     }
 
-    return true; 
+    return true;
 }
-function cleanValidatorMotDePasseMonCompte(){
+function cleanValidatorMotDePasseMonCompte() {
     var inputMotDePasse = document.getElementById('confirmer_new_mot_de_passe');
     inputMotDePasse.setCustomValidity("");
 }
@@ -89,13 +89,15 @@ function cleanValidatorMotDePasseMonCompte(){
 
 
 function voirProduit(titre, description) {
-  alertify.alert(titre, description, function(){ alertify.success('Ok'); })
+    let titreHtml = "<h1 style='text-align: center;'>" + titre + "</h1>";
+    let descriptionHtml = "<h1>" + description + "</h1>";
+
+    alertify.alert(titreHtml, descriptionHtml)
         .setting({
-            'label':'Ok',
-            'message' : description,
-            'onok': function(){ alertify.success('Ok'); }
-        }).setting('closable', false); ;
-    }
+            'label': 'Ok',
+            'closable': false
+        });
+}
 
 erreurMotDePasse();
 
