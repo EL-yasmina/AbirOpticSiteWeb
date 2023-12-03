5f9958b907abb817211fef8059d77eeb2195d55b
EL-Yasmina <yasminataif99@gmail.com>
2023-12-03 18:48:51 +0100

    base de données en ligne

--- back-end/classes-gestion/base-de-donnees.php
+++ back-end/classes-gestion/base-de-donnees.php
@@ -5,7 +5,7 @@ class BaseDeDonnees {
     protected $db;
   
     protected function connexionDb() {
-        $this->db = mysqli_connect("localhost","root","","abiroptic");
+        $this->db = mysqli_connect("sql11.freesqldatabase.com","sql11667198","cdSPCALtRD","sql11667198");
     }
     
     protected function fermerDb(){
